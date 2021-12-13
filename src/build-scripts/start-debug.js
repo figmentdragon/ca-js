f3db8f0ef28d0d95e8fb59e52860f581fec74c79
CJMTermini <75949196+figmentdragon@users.noreply.github.com>
2021-12-12 21:53:50 -0500

    Add files via upload

---
+++ src/build-scripts/start-debug.js
@@ -0,0 +1,24 @@
+const concurrently = require('concurrently');
+const upath = require('upath');
+
+const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
+
+concurrently([
+    { command: 'node --inspect scripts/watch.js', name: 'WATCH', prefixColor: 'bgBlue.bold' },
+    {
+        command: `${browserSyncPath} dist -w --no-online`,
+        name: 'SB_BROWSER_SYNC',
+        prefixColor: 'bgBlue.bold',
+    }
+], {
+    prefix: 'name',
+    killOthers: ['failure', 'success'],
+}).then(success, failure);
+
+function success() {
+    console.log('Success');
+}
+
+function failure() {
+    console.log('Failure');
+}
